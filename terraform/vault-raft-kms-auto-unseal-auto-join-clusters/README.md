b5529d221fcacd572e6fb5bf025447584e8369f6
Jeremy Aranas <jeremy.aranas@hashicorp.com>
2023-05-17 10:37:17 -0700

    added Ubuntu

--- terraform/vault-raft-kms-auto-unseal-auto-join-clusters/README.md
+++ terraform/vault-raft-kms-auto-unseal-auto-join-clusters/README.md
@@ -1,4 +1,7 @@
 # Create Vault nodes on EC2
+
+This is a fork from https://github.com/hashicorp/learn-vault-raft/tree/main/raft-storage/aws
+
 ---
 
 This will spin up 2 clusters (primary and secondary) with 3 Raft nodes in each cluster. This cluster uses AWS KMS auto-unseal and Raft auto-join. Note that creating a KMS key cost $1 each time. 
@@ -16,7 +19,7 @@ This will spin up 2 clusters (primary and secondary) with 3 Raft nodes in each c
 
 
 * Run `export VAULT_ADDR=http://127.0.0.1:8200` on primary_1 and secondary_1
-* Run `vault operator init` on primary_1 and secondary_1 to unseal primary_2, primary_3, secondary_2 and secondary_3
+* Run `vault operator init > init.txt` on primary_1 and secondary_1 to unseal primary_2, primary_3, secondary_2 and secondary_3
 * primary_2, primary_3, secondary_2 and secondary_3 will auto-join cluster
 * Login to primary_1 and secondary_1 `vault login <token>`
 * Run `vault operator raft list-peers` to view cluster
--- terraform/vault-raft-kms-auto-unseal-auto-join-clusters/outputs.tf
+++ terraform/vault-raft-kms-auto-unseal-auto-join-clusters/outputs.tf
@@ -1,28 +1,59 @@
+# Ubuntu
 output "endpoints" {
   value = <<EOF
 
   primary_1 (${aws_instance.primary[0].public_ip}) | internal: (${aws_instance.primary[0].private_ip})
   
-    ssh ec2-user@${aws_instance.primary[0].public_ip} -i ~/Saved/${var.key_name}.cer
+    ssh ubuntu@${aws_instance.primary[0].public_ip} -i ~/Saved/${var.key_name}.cer
    
   primary_2 (${aws_instance.primary[1].public_ip}) | internal: (${aws_instance.primary[1].private_ip})
    
-    ssh ec2-user@${aws_instance.primary[1].public_ip} -i ~/Saved/${var.key_name}.cer
+    ssh ubuntu@${aws_instance.primary[1].public_ip} -i ~/Saved/${var.key_name}.cer
 
   primary_3 (${aws_instance.primary[2].public_ip}) | internal: (${aws_instance.primary[2].private_ip})
     
-    ssh ec2-user@${aws_instance.primary[2].public_ip} -i ~/Saved/${var.key_name}.cer
+    ssh ubuntu@${aws_instance.primary[2].public_ip} -i ~/Saved/${var.key_name}.cer
 
   secondary_1 (${aws_instance.secondary[0].public_ip}) | internal: (${aws_instance.secondary[0].private_ip})
 
-    ssh ec2-user@${aws_instance.secondary[0].public_ip} -i ~/Saved/${var.key_name}.cer
+    ssh ubuntu@${aws_instance.secondary[0].public_ip} -i ~/Saved/${var.key_name}.cer
 
   secondary_2 (${aws_instance.secondary[1].public_ip}) | internal: (${aws_instance.secondary[1].private_ip})
     
-    ssh ec2-user@${aws_instance.secondary[1].public_ip} -i ~/Saved/${var.key_name}.cer
+    ssh ubuntu@${aws_instance.secondary[1].public_ip} -i ~/Saved/${var.key_name}.cer
   
   secondary_3 (${aws_instance.secondary[2].public_ip}) | internal: (${aws_instance.secondary[2].private_ip})
   
-    ssh ec2-user@${aws_instance.secondary[2].public_ip} -i ~/Saved/${var.key_name}.cer
+    ssh ubuntu@${aws_instance.secondary[2].public_ip} -i ~/Saved/${var.key_name}.cer
 EOF
 }
+
+# # RHEL
+# output "endpoints" {
+#   value = <<EOF
+
+#   primary_1 (${aws_instance.primary[0].public_ip}) | internal: (${aws_instance.primary[0].private_ip})
+  
+#     ssh ec2-user@${aws_instance.primary[0].public_ip} -i ~/Saved/${var.key_name}.cer
+   
+#   primary_2 (${aws_instance.primary[1].public_ip}) | internal: (${aws_instance.primary[1].private_ip})
+   
+#     ssh ec2-user@${aws_instance.primary[1].public_ip} -i ~/Saved/${var.key_name}.cer
+
+#   primary_3 (${aws_instance.primary[2].public_ip}) | internal: (${aws_instance.primary[2].private_ip})
+    
+#     ssh ec2-user@${aws_instance.primary[2].public_ip} -i ~/Saved/${var.key_name}.cer
+
+#   secondary_1 (${aws_instance.secondary[0].public_ip}) | internal: (${aws_instance.secondary[0].private_ip})
+
+#     ssh ec2-user@${aws_instance.secondary[0].public_ip} -i ~/Saved/${var.key_name}.cer
+
+#   secondary_2 (${aws_instance.secondary[1].public_ip}) | internal: (${aws_instance.secondary[1].private_ip})
+    
+#     ssh ec2-user@${aws_instance.secondary[1].public_ip} -i ~/Saved/${var.key_name}.cer
+  
+#   secondary_3 (${aws_instance.secondary[2].public_ip}) | internal: (${aws_instance.secondary[2].private_ip})
+  
+#     ssh ec2-user@${aws_instance.secondary[2].public_ip} -i ~/Saved/${var.key_name}.cer
+# EOF
+# }
